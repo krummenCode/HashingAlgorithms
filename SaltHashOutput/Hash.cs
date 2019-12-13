cfe293b614faaeeba060f239096a2b023ae54407
DJ Krummen <44108933+krummenCode@users.noreply.github.com>
2019-12-13 10:10:52 -0600

    Add project files.

---
+++ SaltHashOutput/Hash.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace SaltHashOutput
+{
+    public class Hash
+    {
+        // Additive Hashing
+        public string AdditiveHashing(string value)
+        {
+            // Converts string in Bytes
+            byte[] ascii = Encoding.ASCII.GetBytes(value);
+            StringBuilder additive = new StringBuilder();
+            // for each byte convert to string
+            foreach (Byte b in ascii)
+            {
+                string s = b.ToString();
+                additive.Append(s);
+            }
+            return additive.ToString();
+
+        }
+
+        //public string FoldingHashing(string value)
+        //{
+            // TODO
+            // Split string into chunks of 4 chararcters
+            // Obtain remaining chunk if the inputed string is not divisable by 4
+            // Convert each chunk into a string and add to output string
+           
+        //}
+
+        public string SHA256HashingNoSalt(string rawData)
+        {
+            // Create a SHA256   
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // ComputeHash - returns byte array  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+                // Convert byte array to a string   
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+
+        public string SHA256HashingWithSalt(string value)
+        {
+            string salt = value + DateTime.Now.ToString();
+
+            // Create a SHA256   
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // ComputeHash - returns byte array  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(salt));
+
+                // Convert byte array to a string   
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+
+        }
+    }
+}
---
+++ SaltHashOutput/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaltHashOutput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string selectedOption;
+            string input;
+            Hash hasher = new Hash();
+
+            Console.WriteLine("This program shows what hashing does to an input(password).");
+            Console.WriteLine("What would you like to see hashed?");
+            input = Console.ReadLine();
+            Console.WriteLine("What method of hashing would you like to try: Additive, Folding, SHA256, SHA256 with salt?");
+            selectedOption = Console.ReadLine().ToLower();
+            switch (selectedOption)
+            {
+                case ("additive"):
+                    string r = hasher.AdditiveHashing(input);
+                    Console.WriteLine(r);
+                    break;
+                case ("folding"):
+                    //string f = hasher.FoldingHashing(input);
+                    //Console.WriteLine(f);
+                    Console.WriteLine("Fold is under developement! Sorry for the inconvience, SHA256 is stronger...");
+
+                    break;
+                case ("sha256"):
+                    string sha = hasher.SHA256HashingNoSalt(input);
+                    Console.WriteLine(sha);
+                    break;
+                case ("sha256 with salt"):
+                    string salt = hasher.SHA256HashingWithSalt(input);
+                    Console.WriteLine(salt);
+                    break;
+                default:
+                    Console.WriteLine("Not an option");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
